eb9a3d46019b1e52139739cd4f96238057add4dd
adit <92764174+SubtractIt@users.noreply.github.com>
2021-11-08 14:42:09 -0800

    Add files via upload

---
+++ Prework iOS Dev/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  Prework iOS Dev
+//
+//  Created by Adit Mohindra on 10/25/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+
+    @IBOutlet weak var tipStepper: UIStepper!
+    @IBOutlet weak var tipSegments: UISegmentedControl!
+    @IBOutlet weak var darkThemeSwitcher: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        // Do any additional setup after loading the view.
+    }
+//
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //disposes of any recreated resources
+    }
+//
+    @IBAction func darkTheme(_ sender: Any) {
+       
+    }
+    //
+
+//
+    //copy above for rate 2 and 3
+    
+    
+    
+
+}
